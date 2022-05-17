210a72b66621bd92f41e7fea23d5fa7c01c3be72
Cesar <francacesar26@gmail.com>
2022-05-17 12:45:07 -0300

    retornando o system-user ao get original

--- src/app/view/pages/system-user/system-user.component.ts
+++ src/app/view/pages/system-user/system-user.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { GetAllSystemUserUsecase } from '../../../core/usecases/system-user/get-all-system-user.usecase';
 import { SystemUserModel } from '../../../core/models/system-user.model';
 import { PageResultModel } from '../../../core/utils/responses/page-result.model';
-import { PostSystemUserUsecase } from 'src/app/core/usecases/system-user/post-system-user.usecase';
 
 @Component({
   selector: 'app-system-user',
@@ -12,23 +11,9 @@ import { PostSystemUserUsecase } from 'src/app/core/usecases/system-user/post-sy
 })
 export class SystemUserComponent implements OnInit {
   dataSource!: SystemUserModel[];
-  data?: SystemUserModel;
-  constructor(
-    private getAllSystemUser: GetAllSystemUserUsecase,
-    private postSystemUserUseCase: PostSystemUserUsecase
-  ) {}
+  constructor(private getAllSystemUser: GetAllSystemUserUsecase) {}
 
   ngOnInit(): void {
-    this.data = {
-      email: '',
-      name: '',
-      registerDate: new Date(),
-      registration: '',
-      id: undefined,
-    };
-
-    this.postSystemUserUseCase.execute(this.data).subscribe();
-
     this.getAllSystemUser
       .execute({ pageSize: 20, pageNumber: 1 })
       .subscribe((grid: PageResultModel<SystemUserModel>) => {
